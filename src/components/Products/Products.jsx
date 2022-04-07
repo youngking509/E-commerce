3a2a0e7bac8a54981571bc0903c69c21a98a11a7
kingzy <kingzyv14@gmail.com>
2022-04-07 02:44:59 -0700

    add products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,15 +3,20 @@ import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
 
+import useStyles from './styles'
 
-const products = [
-    { id: 1, name: 'shoes', description: 'Running shoes.', price: '$5' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10' }
-]
 
-const Products = () => {
+// const products = [
+//     { id: 1, name: 'shoes', description: 'Running shoes.', price: '$5' },
+//     { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10' }
+// ]
+
+const Products = ({ products }) => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
